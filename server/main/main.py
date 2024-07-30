fde9b29a63c6165fc492a55f6bc1ce954ea02377
Ryan <ryanpvoong@gmail.com>
2024-07-30 16:12:12 -0400

    setup test environment. added some tests

--- server/main/main.py
+++ server/main/main.py
@@ -1,8 +1,11 @@
+import sys
+
 from fastapi import FastAPI
 
 from main.routers.post import router as post_router
 
 app = FastAPI()
 
-
+print("version:", sys.version)
+print("executable", sys.executable)
 app.include_router(post_router) # prefix="/posts"
