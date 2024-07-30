e5c91fda38339e179f393b6a4e01f70ec28cdac3
Ryan <ryanpvoong@gmail.com>
2024-07-30 10:54:28 -0400

    added routes to get post with comments, and list of comments

--- server/main/routers/post.py
+++ server/main/routers/post.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from main.models.post import Comment, CommentIn, UserPost, UserPostIn
+from main.models.post import (
+    Comment,
+    CommentIn,
+    UserPost,
+    UserPostIn,
+    UserPostWithComments,
+)
 
 router = APIRouter()
 
@@ -10,9 +16,12 @@ async def root():
 post_table = {}
 comment_table = {}
 
+
 def find_post(post_id: int):
   return post_table.get(post_id)
 
+
+# Create post
 @router.post("/post", response_model=UserPost, status_code=201)
 async def create_post(post: UserPostIn):
   data = post.dict()
@@ -22,10 +31,14 @@ async def create_post(post: UserPostIn):
   post_table[last_record_id] = new_post
   return new_post
 
+
+# Get all posts
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
   return list(post_table.values())
 
+
+# Create comment for existing post
 @router.post("/comment", response_model=Comment)
 async def create_comment(comment: CommentIn):
   post = find_post(comment.post_id)
@@ -38,3 +51,21 @@ async def create_comment(comment: CommentIn):
   new_comment = { **data, "id": last_record_id }
   comment_table[last_record_id] = new_comment
   return new_comment
+
+
+# Get ALL comments under post
+@router.get("/post/{post_id}/comment", response_model=list[Comment])
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+# Get post with its comments
+@router.get("/post/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return {"post": post, "comments": await get_comments_on_post(post_id)}
