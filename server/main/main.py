0e8b4abeb8914c652e3540c545cddb10745dd97c
Ryan <ryanpvoong@gmail.com>
2024-08-03 12:47:23 -0400

    created logging schema

--- server/main/main.py
+++ server/main/main.py
@@ -1,13 +1,17 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from main.database import database
+from main.logging_conf import configure_logging
 from main.routers.post import router as post_router
 
-
+logger = logging.getLogger(__name__)
 @asynccontextmanager  # Connect and disconnect from DB
 async def lifespan(app: FastAPI):
+    configure_logging()
+    logger.info("Testing logger")
     await database.connect()
     yield
     await database.disconnect()
