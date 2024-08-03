1a5830e6301574a9f013c80ca9b443ca081cd151
Ryan <ryanpvoong@gmail.com>
2024-08-03 15:08:39 -0400

    added log file handler configurations

--- server/main/logging_conf.py
+++ server/main/logging_conf.py
@@ -13,19 +13,33 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%d %H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "main.log",
+                    "maxBytes": 1024 * 1024,
+                    "backupCount": 2,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": "INFO"},
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 "main": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
                 },
