2a428663853caf0162a6acd3635796248d786759
Ryan <ryanpvoong@gmail.com>
2024-08-06 20:23:33 -0400

    updated get /post route to be able to sort dynamically. added tests to go along with sorts

--- server/main/routers/post.py
+++ server/main/routers/post.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 from typing import Annotated
 
 import sqlalchemy
@@ -12,6 +13,7 @@ from main.models.post import (
     UserPost,
     UserPostIn,
     UserPostWithComments,
+    UserPostWithLikes,
 )
 from main.models.user import User
 from main.security import get_current_user
@@ -53,11 +55,23 @@ async def create_post(
     return {**data, "id": last_record_id}
 
 
+class PostSorting(str, Enum):
+    new = "new"
+    old = "old"
+    most_likes = "most_likes"
+
+
 # Get all posts
-@router.get("/post", response_model=list[UserPost])
-async def get_all_posts():
+@router.get("/post", response_model=list[UserPostWithLikes])
+async def get_all_posts(sorting: PostSorting = PostSorting.new):
     logger.info("Getting all posts")
-    query = post_table.select()
+
+    if sorting == PostSorting.new:
+        query = select_post_and_likes.order_by(post_table.c.id.desc())
+    elif sorting == PostSorting.old:
+        query = select_post_and_likes.order_by(post_table.c.id.asc())
+    elif sorting == PostSorting.most_likes:
+        query = select_post_and_likes.order_by(sqlalchemy.desc("likes"))
 
     logger.debug(query)
 
