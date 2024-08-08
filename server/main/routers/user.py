34219b4ef01daf215b3416b45813cd2c6e943e87
Ryan <ryanpvoong@gmail.com>
2024-08-08 12:05:39 -0400

    added route to confirm user for email confirmations. added tests to also check token validity

--- server/main/routers/user.py
+++ server/main/routers/user.py
@@ -1,12 +1,14 @@
 import logging
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from main.database import database, user_table
 from main.models.user import UserIn
 from main.security import (
     authenticate_user,
     create_access_token,
+    create_confirmation_token,
     get_password_hash,
+    get_subject_for_token_type,
     get_user,
 )
 
@@ -15,7 +17,7 @@ router = APIRouter()
 
 
 @router.post("/register", status_code=201)
-async def register(user: UserIn):
+async def register(user: UserIn, request: Request):
     if await get_user(user.email):  # check if user exists first
         raise HTTPException(
             status_code=400, detail="A user with this email already exists"
@@ -26,7 +28,12 @@ async def register(user: UserIn):
     logger.debug(query)
 
     await database.execute(query)
-    return {"detail": "User created."}
+    return {
+        "detail": "User created. Please confirm your email.",
+        "confirmation_url": request.url_for(
+            "confirm_email", token=create_confirmation_token(user.email)
+        ),
+    }
 
 
 @router.post("/token")
@@ -34,3 +41,16 @@ async def login(user: UserIn):
     user = await authenticate_user(user.email, user.password)
     access_token = create_access_token(user.email)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/confirm/{token}")
+async def confirm_email(token: str):
+    email = get_subject_for_token_type(token, "confirmation")
+    query = (
+        user_table.update().where(user_table.c.email == email).values(confirmed=True)
+    )
+
+    logger.debug(query)
+
+    await database.execute(query)
+    return {"detail": "User confirmed"}
