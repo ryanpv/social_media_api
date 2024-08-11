1d75bfb7426f490e954008ab240df62b49889580
Ryan <ryanpvoong@gmail.com>
2024-08-11 16:23:11 -0400

    added sentry functionality

--- server/main/main.py
+++ server/main/main.py
@@ -4,15 +4,29 @@
 import logging
 from contextlib import asynccontextmanager
 
+import sentry_sdk
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
 
+from main.config import config
 from main.database import database
 from main.logging_conf import configure_logging
 from main.routers.post import router as post_router
 from main.routers.upload import router as upload_router
 from main.routers.user import router as user_router
+
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 
 logger = logging.getLogger(__name__)
 
